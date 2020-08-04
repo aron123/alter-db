3b6cca3ebc23f41e053bf6c7f0da0ec28a09acdb
Kiss, Aron <aron123dev@gmail.com>
2020-08-04 14:30:09 +0200

    Add band prefix filter

--- src/app/alter-db/bands/bands.component.ts
+++ src/app/alter-db/bands/bands.component.ts
@@ -3,6 +3,11 @@ import { BandsService } from './bands.service';
 import { Band } from 'src/app/core/models/band.model';
 import { Router } from '@angular/router';
 
+interface SortedBand {
+  key: string;
+  bands: Band[];
+}
+
 @Component({
   selector: 'app-bands',
   templateUrl: './bands.component.html',
@@ -11,6 +16,8 @@ import { Router } from '@angular/router';
 export class BandsComponent implements OnInit {
 
   bands: Band[];
+  bandsToShow: Band[];
+  sortedBands: SortedBand[];
 
   constructor(
     public bandsService: BandsService,
@@ -18,6 +25,59 @@ export class BandsComponent implements OnInit {
 
   async ngOnInit() {
     this.bands = await this.bandsService.getAllBands();
+    this.bandsToShow = this.bands;
+    this.sortedBands = this.sortBands(this.bands);
+  }
+
+  get bandPrefixes() {
+    if (!this.sortedBands) {
+      return [];
+    }
+
+    return this.sortedBands.map(element => element.key);
+  }
+
+  isStartsWithAlphaNumericChar(char) {
+    const code = char.charCodeAt(0);
+    return (!(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123)) && // lower alpha (a-z)
+      !(code > 192 && code < 670); // latin extended (e.g. Á, É, Ő)
+  }
+
+  sortBands(bands: Band[]): SortedBand[] {
+    const nonAlphaNumeric = { key: '.:?*', bands: [] };
+    const result: SortedBand[] = [];
+
+    for (const band of bands) {
+      if (this.isStartsWithAlphaNumericChar(band.name)) {
+        nonAlphaNumeric.bands.push(band);
+      } else {
+        const samePrefix: SortedBand[] = result.filter(element => element.key === band.name[0]);
+
+        if (samePrefix.length > 0) {
+          samePrefix[0].bands.push(band);
+        } else {
+          result.push({
+            key: band.name[0],
+            bands: [band]
+          });
+        }
+      }
+    }
+
+    result.sort((a, b) => a.key.localeCompare(b.key, 'hu'));
+
+    if (nonAlphaNumeric.bands.length > 0) {
+      result.push(nonAlphaNumeric);
+    }
+
+    return result;
+  }
+
+  listOnlyBandsWithPrefix(prefix: string) {
+    const bandsWithPrefix: SortedBand[] = this.sortedBands.filter(element => element.key === prefix);
+    this.bandsToShow = bandsWithPrefix[0].bands;
   }
 
   goToEditForm(id: number) {
